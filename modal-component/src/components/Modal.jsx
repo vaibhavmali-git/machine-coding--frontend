562945778b8788c859844c0dbaaa5a62d3277d20
vaibhavmali-git <malivaibhav.7620@gmail.com>
2026-07-10 21:53:28 +0530

    feat: build reusable React modal with portal

--- modal-component/src/components/Modal.jsx
+++ modal-component/src/components/Modal.jsx
@@ -1,18 +1,48 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 const Modal = ({ isOpen, onClose }) => {
-  const modalRef = useRef(null);
   const closeBtnRef = useRef(null);
+
+  //Escape Key
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  //focus close button
+  useEffect(() => {
+    if (isOpen) {
+      closeBtnRef.current?.focus();
+    }
+  }, [isOpen]);
 
   if (!isOpen) return null;
 
-
   return createPortal(
     <div className="overlay" onClick={onClose}>
-      <div className="modal" ref={modalRef} aria-modal={true} aria-labelledby="modal-title" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="modal"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-title"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="modal-close">
-          <button onClick={onClose}>x</button>
+          <button ref={closeBtnRef} onClick={onClose} aria-label="Close modal">
+            x
+          </button>
         </div>
         <div className="modal-content">
           <h2 id="modal-title">Modal Title</h2>
