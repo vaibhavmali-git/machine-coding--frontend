466923754d5bd30d655f97af0735497f2fae769f
vaibhavmali-git <malivaibhav.7620@gmail.com>
2026-07-11 13:28:28 +0530

    chore: setup react app with vite

---
+++ otp-login/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OTPInput from './components/OTPInput'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <OTPInput />
+    </div>
+  )
+}
+
+export default App
---
+++ otp-login/src/components/OTPInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OTPInput = () => {
+  return (
+    <div>OTPInput</div>
+  )
+}
+
+export default OTPInput
